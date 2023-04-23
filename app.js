dc6612d5de9b0bfb838d673f1ee526f97486cb0d
Khurshid Ismoilov <95979088+khurshid28@users.noreply.github.com>
2023-04-23 20:16:13 +0500

    Update app.js
    delete helmet

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ app.get("/", (req, res) => res.send("premium pay"));
 const PORT = process.env.PORT || 8090;
 
 // middlewares
-app.use(morgan("dev"), cors(), helmet(), rateLimit(), express.json());
+// app.use(morgan("dev"), cors(), helmet(),rateLimit(), express.json());
+app.use(morgan("dev"), cors(),rateLimit(), express.json());
 
 // auth for APIs
 // app.use(authenticateToken);
