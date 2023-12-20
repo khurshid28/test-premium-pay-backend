d0d96e297b280621de85e7c1dfa2b1567a7c8784
khurshid28 <khurshid_dev@haad.uz>
2023-12-20 14:05:29 +0500

    >>>

--- src/middlewares/error-handler.js
+++ src/middlewares/error-handler.js
@@ -1,7 +1,38 @@
-module.exports = (error, req, res, next) => {
-	if (error.status < 500) {
-		return res.status(error.status).json(error);
-	} else {
-		return next(error);
-	}
+const {
+  AuthorizationError,
+  InvalidTokenError,
+  ValidationError,
+  ForbiddenError,
+  NotFoundError,
+} = require("../utils/errors");
+let axios = require("axios");
+
+module.exports = async (error, req, res, next) => {
+  console.log(error);
+
+  if (
+    !(
+      error instanceof AuthorizationError ||
+      error instanceof InvalidTokenError ||
+      error instanceof ValidationError ||
+      error instanceof ForbiddenError ||
+      error instanceof NotFoundError
+    )
+  ) {
+    let text =
+      "<b>ERROR ON SERVER</b> : %0A<b>" +
+      req.method +
+      " " +
+      req.url +
+      "</b>%0A" +
+      `${JSON.stringify(error.stack)}`.substring(0, 300) +
+      " ...";
+    let url = `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendmessage?chat_id=-${process.env.ERROR_GROUP_ID}&text=${text}&parse_mode=HTML`;
+    let response = await axios.post(url);
+  }
+  if (error.status < 500) {
+    return res.status(error.status).json(error);
+  } else {
+    return next(error);
+  }
 };
