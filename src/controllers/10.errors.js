11f3457f9c8aab1400a0f3439593dee1316f791c
khurshid28 <khurshid_dev@haad.uz>
2023-12-20 15:10:28 +0500

    >>>

---
+++ src/controllers/10.errors.js
@@ -0,0 +1,36 @@
+let {
+  InternalServerError,
+  BadRequestError,
+  ForbiddenError,
+} = require("../utils/errors.js");
+
+let axios = require("axios");
+
+class ErrorController {
+  async sendError(req, res, next) {
+    try {
+      let { message, device } = req.body;
+      let text = "<b>ERROR ON "+ device+"</b> : %0A" + message;
+      let url = `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendmessage?chat_id=-${process.env.ERROR_GROUP_ID}&text=${text}&parse_mode=HTML`;
+      let response = await axios
+        .post(url)
+        .then((response) => response)
+        .catch((err) => {
+          return err;
+        });
+     
+      if (response.status == "200") {
+        return res.status(200).json({
+          message: "message sent successfully",
+        });
+      } else {
+        return next(new BadRequestError(400, "Message didnot send"));
+      }
+    } catch (error) {
+      console.log(error);
+      return next(new InternalServerError(500, error.message));
+    }
+  }
+}
+
+module.exports = new ErrorController();
--- src/routes/_index.js
+++ src/routes/_index.js
@@ -28,6 +28,9 @@ const scoringRouter = require("./8.scoring");
 
 const supportRouter = require("./9.support");
 
+const errorRouter = require("./10.errors");
+
+
 router.use("/api/v1/login", loginRouter);
 router.use("/api/v1/myid", myidRouter);
 router.use("/api/v1/user", userRouter);
@@ -43,6 +46,7 @@ router.use("/api/v1/fillial", fillialRouter);
 
 router.use("/api/v1/scoring",scoringRouter );
 router.use("/api/v1/support",supportRouter );
+router.use("/api/v1/error",errorRouter );
 
 router.use("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
