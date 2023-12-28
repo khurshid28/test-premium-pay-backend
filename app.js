66f986eb783a079acfc9ed6cd64c4f309b23f8d3
khurshid28 <khurshid_dev@haad.uz>
2023-12-28 07:48:12 +0500

    >>

--- app.js
+++ app.js
@@ -139,6 +139,93 @@ app.get("/test/droptable", (req, res) => {
 // starting server
 app.listen(PORT, async () => {
   console.log(`server ready on port:${PORT}`);
+
+  db.query(`DROP TABLE Zayavka;`, function (err, results, fields) {
+    if (err) {
+      
+      return res.send({ err });
+    }
+    db.query(PREMIUM.createZayavkaTable, function (err, results, fields) {
+      console.log(err);
+      if (err) {
+        console.log({ err });
+      }
+      
+      console.log({ results });
+    });
+    return res.send({ results });
+  });
+
+
+  db.query(`DROP TABLE merchant;`, function (err, results, fields) {
+    if (err) {
+      
+      return res.send({ err });
+    }
+    db.query(PREMIUM.createMerchantTable, function (err, results, fields) {
+      console.log(err);
+      if (err) {
+        console.log({ err });
+      }
+      
+      console.log({ results });
+    });
+    return res.send({ results });
+  });
+
+
+  db.query(`DROP TABLE fillial;`, function (err, results, fields) {
+    if (err) {
+      
+      return res.send({ err });
+    }
+    db.query(PREMIUM.createFillialTable, function (err, results, fields) {
+      console.log(err);
+      if (err) {
+        console.log({ err });
+      }
+      
+      console.log({ results });
+    });
+    return res.send({ results });
+  });
+
+
+
+ 
+  db.query(`DROP TABLE FillialAdmin;`, function (err, results, fields) {
+    if (err) {
+      
+      return res.send({ err });
+    }
+    db.query(PREMIUM.createFillialAdminTable, function (err, results, fields) {
+      console.log(err);
+      if (err) {
+        console.log({ err });
+      }
+      
+      console.log({ results });
+    });
+    return res.send({ results });
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   // db.query(PREMIUM.createZayavkaTable, function (err, results, fields) {
   //   console.log(err);
   //   if (err) {
