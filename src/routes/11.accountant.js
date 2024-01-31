c546aad0284c651409b6462118e632d0314376b4
khurshid28 <khurshid_dev@haad.uz>
2024-01-31 18:01:57 +0500

    last insha Alloh

--- src/routes/11.accountant.js
+++ src/routes/11.accountant.js
@@ -4,6 +4,6 @@ const accountantController = require("../controllers/11.accountant.js");
 const router = Router();
 
 router.post("/create", accountantController.create);
-router.get("/pay/", accountantController.pay);
+router.post("/pay/", accountantController.pay);
 
 module.exports = router;
