86a54b05de1cc94e97c930ff5e19fcd6531bc049
khurshid28 <khurshid_dev@haad.uz>
2024-02-29 18:06:48 +0500

    route v2 version

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -107,20 +107,20 @@ bot.on("message", async (msg) => {
             },
           ];
           
-          for (let i = 1; i < results.length; i++) {
-            if (results[i].date == res[-1].date) {
-              res[-1].fullname += ("\n"+ results[i].fullname);
-            } else {
-              res.push(results[i]);
-            }
+          // for (let i = 1; i < results.length; i++) {
+          //   if (results[i].date == res[-1].date) {
+          //     res[-1].fullname += ("\n"+ results[i].fullname);
+          //   } else {
+          //     res.push(results[i]);
+          //   }
       
-            check = results[i].status == "canceled_by_scoring";
-            res[-1] = {
-              ...res[-1],
-              canceled: (check ? 1 : 0) + res[-1].canceled,
-              finished: (!check ? 1 : 0) + res[-1].finished,
-            };
-          }
+          //   check = results[i].status == "canceled_by_scoring";
+          //   res[-1] = {
+          //     ...res[-1],
+          //     canceled: (check ? 1 : 0) + res[-1].canceled,
+          //     finished: (!check ? 1 : 0) + res[-1].finished,
+          //   };
+          // }
           console.log(res);
           console.log(res[-1]);
         }
