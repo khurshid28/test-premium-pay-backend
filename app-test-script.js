6799bd900f2e10a9afe9cf4f8c1e991bc1634d4a
khurshid28 <khurshid_dev@haad.uz>
2024-03-01 16:17:21 +0500

    app-test-script

---
+++ app-test-script.js
@@ -0,0 +1,46 @@
+let db = require("./src/config/db");
+
+let data = new Promise(function (resolve, reject) {
+    db.query(
+      `SELECT id,fullname,status,Date(created_time - interval 5 hour) as date  from Zayavka WHERE  status in ('canceled_by_scoring','finished','paid') and  ('2024-01-31' < Date(created_time - interval 5 hour) and Date(created_time - interval 5 hour) < '2024-03-01')`,
+      function (err, results, fields) {
+        if (err) {
+          resolve(null);
+          return null;
+        }
+        return resolve(results);
+      }
+    );
+  });
+  
+  data.then(function (results) {
+    if (results && results.length) {
+      let check = results[0].status == "canceled_by_scoring";
+      let res = [
+        {
+          ...results[0],
+          canceled: check ? 1 : 0,
+          finished: !check ? 1 : 0,
+        },
+      ];
+      
+      for (let i = 1; i < results.length; i++) {
+        if (results[i].date == res[res.length - 1].date) {
+          res[res.length - 1].fullname += ("\n"+ results[i].fullname);
+        } else {
+          res.push(results[i]);
+        }
+  
+        check = results[i].status == "canceled_by_scoring";
+        res[res.length - 1] = {
+          ...res[res.length - 1],
+          canceled: (check ? 1 : 0) + (res[res.length - 1].canceled ?? 0),
+          finished: (!check ? 1 : 0) + (res[res.length - 1].finished ?? 0),
+        };
+      }
+      // console.log(res);
+      console.log(res);
+    }
+    
+    
+  }).catch((err)=>console.log(err));
--- src/bot/bot.js
+++ src/bot/bot.js
@@ -82,72 +82,72 @@ bot.on("message", async (msg) => {
 
     // }
 
-    if (msg.text == "/get" && chatId === 2053690211) {
-      let data = new Promise(function (resolve, reject) {
-        db.query(
-          `SELECT id,fullname,status,Date(created_time - interval 5 hour) as date  from Zayavka WHERE  status in ('canceled_by_scoring','finished','paid') and  ('2024-01-31' < Date(created_time - interval 5 hour) and Date(created_time - interval 5 hour) < '2024-03-01')`,
-          function (err, results, fields) {
-            if (err) {
-              resolve(null);
-              return null;
-            }
-            return resolve(results);
-          }
-        );
-      });
+    // if (msg.text == "/get" && chatId === 2053690211) {
+    //   let data = new Promise(function (resolve, reject) {
+    //     db.query(
+    //       `SELECT id,fullname,status,Date(created_time - interval 5 hour) as date  from Zayavka WHERE  status in ('canceled_by_scoring','finished','paid') and  ('2024-01-31' < Date(created_time - interval 5 hour) and Date(created_time - interval 5 hour) < '2024-03-01')`,
+    //       function (err, results, fields) {
+    //         if (err) {
+    //           resolve(null);
+    //           return null;
+    //         }
+    //         return resolve(results);
+    //       }
+    //     );
+    //   });
       
-      data.then(function (results) {
-        if (results && results.length) {
-          let check = results[0].status == "canceled_by_scoring";
-          let res = [
-            {
-              ...results[0],
-              canceled: check ? 1 : 0,
-              finished: !check ? 1 : 0,
-            },
-          ];
+    //   data.then(function (results) {
+    //     if (results && results.length) {
+    //       let check = results[0].status == "canceled_by_scoring";
+    //       let res = [
+    //         {
+    //           ...results[0],
+    //           canceled: check ? 1 : 0,
+    //           finished: !check ? 1 : 0,
+    //         },
+    //       ];
           
-          for (let i = 1; i < results.length; i++) {
-            if (results[i].date == res[res.length - 1].date) {
-              res[res.length - 1].fullname += ("\n"+ results[i].fullname);
-            } else {
-              res.push(results[i]);
-            }
+    //       for (let i = 1; i < results.length; i++) {
+    //         if (results[i].date == res[res.length - 1].date) {
+    //           res[res.length - 1].fullname += ("\n"+ results[i].fullname);
+    //         } else {
+    //           res.push(results[i]);
+    //         }
       
-            check = results[i].status == "canceled_by_scoring";
-            res[res.length - 1] = {
-              ...res[res.length - 1],
-              canceled: (check ? 1 : 0) + (res[res.length - 1].canceled ?? 0),
-              finished: (!check ? 1 : 0) + (res[res.length - 1].finished ?? 0),
-            };
-          }
-          // console.log(res);
-          console.log(res);
-        }
+    //         check = results[i].status == "canceled_by_scoring";
+    //         res[res.length - 1] = {
+    //           ...res[res.length - 1],
+    //           canceled: (check ? 1 : 0) + (res[res.length - 1].canceled ?? 0),
+    //           finished: (!check ? 1 : 0) + (res[res.length - 1].finished ?? 0),
+    //         };
+    //       }
+    //       // console.log(res);
+    //       console.log(res);
+    //     }
         
-      });
+    //   });
       
-      // let zayavkalar = await new Promise(function (resolve, reject) {
-      //   db.query(
-      //     `SELECT id,fullname,status,Date(created_time -interval 5 hour) as date from Zayavka where status in("finished","paid","progress","canceled_by_scoring") and id>55;`,
-      //     function (err, results, fields) {
-      //       if (err) {
-      //         resolve(null);
-      //         return null;
-      //       }
-      //       return resolve(results);
-      //     }
-      //   );
-      // });
-      // console.log(zayavkalar.length);
-      // for (let index = 0; index < zayavkalar.length; index++) {
-      //   const element = zayavkalar[index];
-      //   await bot.sendMessage(
-      //     chatId,
-      //     `ID : ${element.id} \nFULLNAME : ${element.fullname} \nSTATUS : ${element.status}\nDATE : ${element.date}`
-      //   );
-      // }
-    }
+    //   // let zayavkalar = await new Promise(function (resolve, reject) {
+    //   //   db.query(
+    //   //     `SELECT id,fullname,status,Date(created_time -interval 5 hour) as date from Zayavka where status in("finished","paid","progress","canceled_by_scoring") and id>55;`,
+    //   //     function (err, results, fields) {
+    //   //       if (err) {
+    //   //         resolve(null);
+    //   //         return null;
+    //   //       }
+    //   //       return resolve(results);
+    //   //     }
+    //   //   );
+    //   // });
+    //   // console.log(zayavkalar.length);
+    //   // for (let index = 0; index < zayavkalar.length; index++) {
+    //   //   const element = zayavkalar[index];
+    //   //   await bot.sendMessage(
+    //   //     chatId,
+    //   //     `ID : ${element.id} \nFULLNAME : ${element.fullname} \nSTATUS : ${element.status}\nDATE : ${element.date}`
+    //   //   );
+    //   // }
+    // }
 
     if (msg.text == "/data") {
       zayavkalar1 = await new Promise(function (resolve, reject) {
@@ -511,3 +511,5 @@ function toMoney(number) {
   }
   return result;
 }
+
+module.exports = bot
