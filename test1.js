7c5f127618ba7639e4c216a8a559f80a477a6667
khurshid28 <khurshid_dev@haad.uz>
2024-03-06 23:31:07 +0500

    >

--- test1.js
+++ test1.js
@@ -3,124 +3,124 @@ let db = require("./src/config/db");
     
       let data = [];
       let banks = ["Davr", "Hamkor", "Asaka", "QQB"];
-      () => {
-        banks.forEach(async (item, index) => {
-          let zayavkalarUspeshna = await new Promise(function (
-            resolve,
-            reject
-          ) {
+    //   () => {
+    //     banks.forEach(async (item, index) => {
+    //       let zayavkalarUspeshna = await new Promise(function (
+    //         resolve,
+    //         reject
+    //       ) {
 
-            db.query(
-              `SELECT count(id) from Zayavka where step=8  and bank='${item}'`,
-              function (err, results, fields) {
-                if (err) {
-                    console.log(err);
-                  resolve(null);
-                  return null;
-                }
-                console.log(results);
-                return resolve(results);
-              }
-            );
-          });
+    //         db.query(
+    //           `SELECT count(id) from Zayavka where step=8  and bank='${item}'`,
+    //           function (err, results, fields) {
+    //             if (err) {
+    //                 console.log(err);
+    //               resolve(null);
+    //               return null;
+    //             }
+    //             console.log(results);
+    //             return resolve(results);
+    //           }
+    //         );
+    //       });
 
 
-          let zayavkalarScoringOtkaz = await new Promise(function (
-            resolve,
-            reject
-          ) {
-            db.query(
-              `SELECT count(id) from Zayavka where status='canceled_by_scoring' and bank=${item}`,
-              function (err, results, fields) {
-                if (err) {
-                    console.log(err);
-                  resolve(null);
-                  return null;
-                }
-                return resolve(results);
-              }
-            );
-          });
-          let zayavkalarClienttOtkaz = await new Promise(function (
-            resolve,
-            reject
-          ) {
-            db.query(
-              `SELECT count(id) from Zayavka where status='canceled_by_client' and bank=${item}`,
-              function (err, results, fields) {
-                if (err) {
-                    console.log(err);
-                  resolve(null);
-                  return null;
-                }
-                return resolve(results);
-              }
-            );
-          });
+    //       let zayavkalarScoringOtkaz = await new Promise(function (
+    //         resolve,
+    //         reject
+    //       ) {
+    //         db.query(
+    //           `SELECT count(id) from Zayavka where status='canceled_by_scoring' and bank=${item}`,
+    //           function (err, results, fields) {
+    //             if (err) {
+    //                 console.log(err);
+    //               resolve(null);
+    //               return null;
+    //             }
+    //             return resolve(results);
+    //           }
+    //         );
+    //       });
+    //       let zayavkalarClienttOtkaz = await new Promise(function (
+    //         resolve,
+    //         reject
+    //       ) {
+    //         db.query(
+    //           `SELECT count(id) from Zayavka where status='canceled_by_client' and bank=${item}`,
+    //           function (err, results, fields) {
+    //             if (err) {
+    //                 console.log(err);
+    //               resolve(null);
+    //               return null;
+    //             }
+    //             return resolve(results);
+    //           }
+    //         );
+    //       });
 
-          let zayavkalarTimeOtkaz = await new Promise(function (
-            resolve,
-            reject
-          ) {
-            db.query(
-              `SELECT count(id) from Zayavka where status='canceled_by_daily' and bank=${item}`,
-              function (err, results, fields) {
-                if (err) {
-                    console.log(err);
-                  resolve(null);
-                  return null;
-                }
-                return resolve(results);
-              }
-            );
-          });
-          let getZayavka = await new Promise(function (resolve, reject) {
-            db.query(
-              `SELECT count(id) from Zayavka where bank=${item}  status not in("progress")`,
-              function (err, results, fields) {
-                if (err) {
-                    console.log(err);
-                  resolve(null);
-                  return null;
-                }
-                return resolve(results);
-              }
-            );
-          });
-          if (getZayavka) {
-            let percentUspeshna = (zayavkalarUspeshna / getZayavka) * 100;
-            let percentScoring = (zayavkalarScoringOtkaz / getZayavka) * 100;
-            let percentClient = (zayavkalarClienttOtkaz / getZayavka) * 100;
-            let percentTime = (zayavkalarTimeOtkaz / getZayavka) * 100;
-            data.push({
-              name: item,
-              statistika: {
-                success: {
-                  percent: percentUspeshna,
-                  count: zayavkalarUspeshna,
-                },
-                scoring_otkaz: {
-                  percent: percentScoring,
-                  count: zayavkalarScoringOtkaz,
-                },
-                client_otkaz: {
-                  percent: percentClient,
-                  count: zayavkalarClienttOtkaz,
-                },
-                time_otkaz: {
-                  percent: percentTime,
-                  count: zayavkalarTimeOtkaz,
-                },
-              },
-            });
-          } else {
-            data.push({
-              name: item,
-              statistika: null,
-            });
-          }
-        });
-      };
+    //       let zayavkalarTimeOtkaz = await new Promise(function (
+    //         resolve,
+    //         reject
+    //       ) {
+    //         db.query(
+    //           `SELECT count(id) from Zayavka where status='canceled_by_daily' and bank=${item}`,
+    //           function (err, results, fields) {
+    //             if (err) {
+    //                 console.log(err);
+    //               resolve(null);
+    //               return null;
+    //             }
+    //             return resolve(results);
+    //           }
+    //         );
+    //       });
+    //       let getZayavka = await new Promise(function (resolve, reject) {
+    //         db.query(
+    //           `SELECT count(id) from Zayavka where bank=${item}  status not in("progress")`,
+    //           function (err, results, fields) {
+    //             if (err) {
+    //                 console.log(err);
+    //               resolve(null);
+    //               return null;
+    //             }
+    //             return resolve(results);
+    //           }
+    //         );
+    //       });
+    //       if (getZayavka) {
+    //         let percentUspeshna = (zayavkalarUspeshna / getZayavka) * 100;
+    //         let percentScoring = (zayavkalarScoringOtkaz / getZayavka) * 100;
+    //         let percentClient = (zayavkalarClienttOtkaz / getZayavka) * 100;
+    //         let percentTime = (zayavkalarTimeOtkaz / getZayavka) * 100;
+    //         data.push({
+    //           name: item,
+    //           statistika: {
+    //             success: {
+    //               percent: percentUspeshna,
+    //               count: zayavkalarUspeshna,
+    //             },
+    //             scoring_otkaz: {
+    //               percent: percentScoring,
+    //               count: zayavkalarScoringOtkaz,
+    //             },
+    //             client_otkaz: {
+    //               percent: percentClient,
+    //               count: zayavkalarClienttOtkaz,
+    //             },
+    //             time_otkaz: {
+    //               percent: percentTime,
+    //               count: zayavkalarTimeOtkaz,
+    //             },
+    //           },
+    //         });
+    //       } else {
+    //         data.push({
+    //           name: item,
+    //           statistika: null,
+    //         });
+    //       }
+    //     });
+    //   };
 
       for await (let item of banks) {
         let zayavkalarUspeshna = await new Promise(function (resolve, reject) {
