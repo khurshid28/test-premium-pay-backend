5ebb780e3255008b9e71289482e4f7137d2c8c16
khurshid28 <khurshid_dev@haad.uz>
2024-04-15 13:20:41 +0500

    >

--- src/controllers/6.app.js
+++ src/controllers/6.app.js
@@ -1706,10 +1706,10 @@ class App {
       
       
         console.log(val);
-        console.log(Math.floor( req.body.payment_amount / 100000));
-        console.log(Math.floor(zayavkaOld.amount * (1 + val / 100)/100000));
-        console.log( Math.floor( req.body.payment_amount / 100000)  !=  Math.floor(zayavkaOld.amount * (1 + val / 100)/100000));
-        if ( Math.floor( req.body.payment_amount / 100000)  !=  Math.floor(zayavkaOld.amount * (1 + val / 100)/100000)) {
+        console.log(Math.floor( req.body.payment_amount / 1000));
+        console.log(Math.floor(zayavkaOld.amount * (1 + val["percent"] / 100)/1000));
+        console.log( Math.floor( req.body.payment_amount / 1000)  !=  Math.floor(zayavkaOld.amount * (1 + val["percent"] / 100)/1000));
+        if ( Math.floor( req.body.payment_amount / 1000)  !=  Math.floor(zayavkaOld.amount * (1 + val["percent"] / 100)/1000)) {
          return next(new BadRequestError(400, "Payment amount Error"));
         } 
       } 
